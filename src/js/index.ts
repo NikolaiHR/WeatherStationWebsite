69210cd85c1e0ad40aba410f1e6d9c5382c636c9
Andu972k <ande972k@edu.easj.dk>
2019-11-27 13:24:03 +0100

    Update GetCurrentWeather

--- src/js/index.ts
+++ src/js/index.ts
@@ -15,6 +15,28 @@ function GetCurrentWeather(): void {
     let currentTempSpanElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("currentTemp");
     let currentHumiditySpanElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("currentHumidity");
     let currentPressureSpanElement: HTMLSpanElement = <HTMLSpanElement>document.getElementById("currentPressure");
-     
+
+axios.get<IForecast>(baseURI + "/Current")
+.then(function (response: AxiosResponse): void {
+    let currentForecast: IForecast = response.data;
+    currentTempSpanElement.innerHTML = currentForecast.temp.toString();
+    currentHumiditySpanElement.innerHTML = currentForecast.humidity.toString();
+    currentPressureSpanElement.innerHTML = currentForecast.pressure.toString();
+})
+.catch(function (error: AxiosError): void {
+if (error.response){
+    currentTempSpanElement.innerHTML = error.message;
+    currentHumiditySpanElement.innerHTML = " ";
+    currentPressureSpanElement.innerHTML = " ";
+}
+else {
+    currentTempSpanElement.innerHTML = error.message;
+    currentHumiditySpanElement.innerHTML = " ";
+    currentPressureSpanElement.innerHTML = " ";
+}
+})
+
 }
 
+let currentButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("currentWeatherButton");
+currentButton.addEventListener("click", GetCurrentWeather);
